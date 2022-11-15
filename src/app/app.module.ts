a0b85d66be4453b5a2a8035e5b4012de9b435ada
serraguti <pacoserranox@gmail.com>
2022-11-15 14:27:59 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaproductosComponent } from './components/listaproductos/listaproductos.component';
+import { HijococheComponent } from './components/hijocoche/hijocoche.component';
+import { PadrecochesComponent } from './components/padrecoches/padrecoches.component';
+import { PadredeportesComponent } from './components/padredeportes/padredeportes.component';
+import { HijodeporteComponent } from './components/hijodeporte/hijodeporte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaproductosComponent,
+    HijococheComponent,
+    PadrecochesComponent,
+    PadredeportesComponent,
+    HijodeporteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/padredeportes/padredeportes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-padredeportes',
+  templateUrl: './padredeportes.component.html',
+  styleUrls: ['./padredeportes.component.css']
+})
+export class PadredeportesComponent implements OnInit {
+  public deportes: Array<string>;
+  public mensaje!: string;
+  //CUANDO HAGAMOS EMIT, LEERA ESTE METODO
+  seleccionarDeportePadre(event: string): void {
+    console.log(event);
+    this.mensaje = "Deporte Seleccionado: " + event + "!!!";
+  }
+
+  constructor() { 
+    this.deportes = ["Petanca", "Padel", "Curling", "Cometas"];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
